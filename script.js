ab65b26b38d0e94286cdcc91f78c0a3394e2c93d
Kai Pratt <68077936+kaidonnerpratt@users.noreply.github.com>
2025-06-04 09:30:34 -0500

    kai / form function test

---
+++ script.js
@@ -0,0 +1,26 @@
+var quiz = document.getElementById("quiz")
+var res = document.getElementById("results")
+
+function findscore(event){
+
+    event.preventDefault()
+
+    var form = $("form")
+
+    var list = form.serializeArray();
+
+    console.log(list)
+
+    var correct = 0
+
+    for (var i of list){
+        if (i.value == "c"){
+            correct++
+        }
+    }
+
+    res.innerHTML =((Math.round((correct/list.length)*10))/10)+"%"
+
+} 
+
+quiz.addEventListener("submit", findscore)
